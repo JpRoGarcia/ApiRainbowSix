e9dd3635cd5f4300f68d710f7532cc27a1ed59c5
Juan Pablo Rojas García <juan.1020405@gmail.com>
2021-03-30 18:36:00 -0500

    Creacion de Controladore Creacion de Routers Creacion de Servicion

---
+++ Api/controllers/users.controller.js
@@ -0,0 +1,16 @@
+const _pg = require("../services/postgres.service"); 
+
+const getUsers = async (req, res) => {
+    try {
+        let sql = "SELECT * FROM usuarios";
+        let response_db = await _pg.execute(sql);
+        let rows = response_db.rows;
+        return res.send(rows);
+    } catch (error) {
+        console.error(error);
+        return res.send(error);
+    }
+
+}
+
+module.exports = { getUsers };
---
+++ Api/services/postgres.service.js
@@ -0,0 +1,18 @@
+const pg = require("pg");
+
+const execute = async (sql) => {
+    const client = new pg.Client({
+        user: 'postgres',
+        host: 'localhost',
+        database: 'test',
+        password: '07150126',
+        port: 5432,
+      });
+
+    await client.connect();
+    const res = await client.query(sql);
+    await client.end();
+    return res
+};
+
+module.exports = { execute };
