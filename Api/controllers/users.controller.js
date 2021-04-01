ef268c43210725ffeb3e6b32d24bf708bca5f919
Juan Pablo Rojas García <juan.1020405@gmail.com>
2021-03-31 23:40:41 -0500

    Agregar Put Agregar Delete Agregar Save

--- Api/controllers/users.controller.js
+++ Api/controllers/users.controller.js
@@ -10,7 +10,84 @@ const getUsers = async (req, res) => {
         console.error(error);
         return res.send(error);
     }
-
 }
 
-module.exports = { getUsers };
+const saveUsers = async (req, res) => {
+    try {
+        let user = req.body;
+        let sql = `INSERT INTO public.usuarios
+        (idusuario, numero_partida, rol_favorito, arma_favorita, 
+            baja_confirmada, muerte, municion_gastada, municion_impactada)
+        VALUES('${user.idusuario}', '${user.numero_partida}', '${user.rol_favorito}', 
+        '${user.arma_favorita}', ${user.baja_confirmada}, ${user.muerte}, 
+        ${user.municion_gastada}, ${user.municion_impactada});
+        `
+        await _pg.execute(sql);
+
+        return res.send({
+            ok: true,
+            message: "Usuario creado",
+            info: user,
+        });
+    } catch (error) {
+        return res.send({
+            ok: false,
+            message: "Error al crear el usuario",
+            info: error,
+    });
+    }
+}
+
+const deleteUsers = async (req, res) => {
+    try {
+        let idusuario = req.params.idusuario;
+        let sql = `DELETE FROM public.usuarios WHERE idusuario='${idusuario}';`;
+        let response_db = await _pg.execute(sql);
+        let row_count = response_db.rowCount;
+        return res.send({
+            ok: row_count == 1 ? true : false,
+            message: "Usuario eliminado",
+            info: idusuario,
+        });
+       } catch (error) {
+        return res.send({
+          ok: false,
+          message: "Error al eliminar el usuario",
+          info: error,
+        });
+    }  
+}
+
+const updateUsers = async (req, res) => {
+    try {
+      let idusuario = req.params.idusuario;
+      let user = req.body;
+
+      /* numero_partida='${user.numero_partida}', , baja_confirmada=${user.baja_confirmada}, 
+      muerte=${user.muerte}, municion_gastada=${user.municion_gastada}, 
+      municion_impactada=${user.municion_impactada}
+      */
+
+      let sql = `UPDATE public.usuarios
+      SET rol_favorito='${user.rol_favorito}', arma_favorita='${user.arma_favorita}'
+      WHERE idusuario='${idusuario}';`;
+
+      let response_db = await _pg.execute(sql);
+      let row_count = response_db.rowCount;
+      return res.send({
+        ok: row_count == 1 ? true : false,
+        message: "Usuario actualizado",
+        info: idusuario,
+      });
+    } catch (error) {
+      console.error(error);
+      return res.send({
+        ok: false,
+        message: "Error al actualizar el usuario",
+        info: error,
+      });
+    }
+  };
+
+
+module.exports = { getUsers, saveUsers, deleteUsers, updateUsers};
--- Api/routers/index.js
+++ Api/routers/index.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const userController = require("../controllers/users.controller")
 
 router.get("/usuarios", userController.getUsers);
+router.post("/usuarios", userController.saveUsers);
+router.delete("/usuarios/:idusuario", userController.deleteUsers);
+router.put("/usuarios/:idusuario", userController.updateUsers);
 
 module.exports = router
 
