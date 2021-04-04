5ecc397dae489fdddec5f5da9f79155718231918
Juan Pablo Rojas García <juan.1020405@gmail.com>
2021-04-03 21:39:27 -0500

    Agregar Ruta del Usuario Nuevo

--- Api/routers/index.js
+++ Api/routers/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/users.controller")
+const randomController = require("../controllers/randomUsers.controller")
 
 router.get("/usuarios", userController.getUsers);
 router.post("/usuarios", userController.saveUsers);
+router.post("/usuariosRandom", randomController.saveRandomUsers);
 router.delete("/usuarios/:idusuario", userController.deleteUsers);
 router.put("/usuarios/:idusuario", userController.updateUsers);
 
