62ee2411cca30890213d6bb9a4a31354c27760bd
Juan Pablo Rojas García <juan.1020405@gmail.com>
2021-04-06 20:40:36 -0500

    Agregacion de Rutas

--- Api/routers/index.js
+++ Api/routers/index.js
@@ -2,11 +2,18 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/users.controller")
 const randomController = require("../controllers/randomUsers.controller")
+const ReportsController = require("../controllers/reports.controller");
 
-router.get("/usuarios", userController.getUsers);
+router.get("/reportPorcenKoD", ReportsController.getReporPorcenjeKoD);
+router.get("/reportPorcenPrecision", ReportsController.getReporPorcenPrecision);
+
 router.post("/usuarios", userController.saveUsers);
 router.post("/usuariosRandom", randomController.saveRandomUsers);
+
+router.get("/usuarios", userController.getUsers);
+
 router.delete("/usuarios/:idusuario", userController.deleteUsers);
+
 router.put("/usuarios/:idusuario", userController.updateUsers);
 
 module.exports = router
